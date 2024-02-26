2cab03d050f21852bda5773f4434255fa71651bf
ss <ss@gmail.com>
2024-02-26 18:46:16 +0530

    file to delete old logs

---
+++ oldfile-log.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+source=/tmp/log-files
+
+if [ ! -d $source ]
+then
+    echo -e "$R $source directory not exists $N"
+    exit 1
+fi
+
+files-delete=$(find $source -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+  echo "deleting file: $line"
+  rm -rf $line
+done <<< $files-delete
