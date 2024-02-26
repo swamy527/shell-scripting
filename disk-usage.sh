b3a8d10a87464a70a37c989e2cf8bb67eb83eb76
ss <ss@gmail.com>
2024-02-26 20:14:46 +0530

    file to delete old logs

---
+++ disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+disk_usage=$(df -hT | grep -vE 'tmp|File')
+Thresh=1
+message=""
+
+while IFS= read line
+do
+  usage=$(echo $line | awk '{print6F}' | cut -d % -f1)
+  partition=$(echo $line | awk '{print1F}')
+  if [ $usage -ge $Thresh ]
+  then
+      message+="high disk usage on $partition: $usage\n"
+   fi
+done <<< $disk_usage
+
+echo -e "$message"
