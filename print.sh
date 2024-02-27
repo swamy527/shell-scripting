2f4eddc7d6a7d0d04ab8be02c09302b6391892a8
ss <ss@gmail.com>
2024-02-27 09:02:20 +0530

    new file created

--- print.sh
+++ print.sh
@@ -7,8 +7,9 @@ then
     echo -e "$file not exists"
 fi
 
-while IFS=":" read -r username password
+while IFS=":" read -r username password userid groupid
 do
   echo -e " user name is : $username"
   echo -e "password is : $password"
+  echo -e "user id : $userid"
 done < $file
